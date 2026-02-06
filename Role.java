ae19654c1225f4a74c8eb29d09138264ab5ed44d
Daisy <nolascd2@wwu.edu>
2026-02-06 00:51:24 -0800

    Completed Role class

--- Role.java
+++ Role.java
@@ -1,21 +1,58 @@
 public class Role {
     private String roleName;
+    private String line;
     private int requiredRank;
-    private boolean isRankAvailible;
-    Player assignedPlayer;
-    public Role(){
-        
-    }
-   public Role(String roleName,int requiredRank){
-    this.roleName=roleName;
-    this.requiredRank=requiredRank;
-    }
+    private boolean isStarringRole;
+    private Player assignedPlayer;
 
-}
-class Star extends Role {
-
-    public Star(){
+    public Role(String roleName, String line, int requiredRank) {
+        this.roleName = roleName;
+        this.line = line;
+        this.requiredRank = requiredRank;
+        this.assignedPlayer=null;
 
     }
-    
+    //Name role name on scenecard 
+    public String getRoleName() {
+        return roleName;
+
+    }
+
+    // getting acting line
+    public String getLine() {
+        return line;
+
+    }
+    //  needed rank for on card roles
+    public int getRequiredRank() {
+        return requiredRank;
+
+    }
+    // check if role is starring role
+    public boolean isStarringRole() {
+        return isStarringRole;
+    }
+    //get player on that role
+    public Player getAssignnedPlayer() {
+        return assignedPlayer;
+
+    }
+
+    // sets player to role
+    public void setAssignnedPlayer(Player player) {
+        this.assignedPlayer = player;
+
+    }
+
+    // checks if role is availible
+    public boolean isOccupied() {
+        return assignedPlayer != null;
+    }
+
+    // set role to occupied once player has taken role
+    public void setOccupied(boolean Occupied) {
+      setAssignnedPlayer(assignedPlayer);
+
+    }
+
 }
