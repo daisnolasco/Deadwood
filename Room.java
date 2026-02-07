3a3b7ca6eb26e1960abfc2bca0121edece3c2c51
Immanuel <tesfayi@wwu.edu>
2026-02-06 19:11:19 -0800

    Add CastingOffice class and other changes

--- Room.java
+++ Room.java
@@ -1,4 +1,12 @@
-public class Room {
+// Add this to a separate file or at the bottom of CastingOffice.java for testing
+class Room {
+    private String name;
     
+    public Room(String name) {
+        this.name = name;
+    }
     
+    public String getName() {
+        return name;
+    }
 }
