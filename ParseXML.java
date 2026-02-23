2bbb5b1cddedc70bda3f8f8a0f779d52a4f15aec
Daisy <nolascd2@wwu.edu>
2026-02-22 23:55:29 -0800

    parser

--- ParseXML.java
+++ ParseXML.java
@@ -5,8 +5,6 @@ import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.Node;
 import org.w3c.dom.Element;
-import java.io.File;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 
